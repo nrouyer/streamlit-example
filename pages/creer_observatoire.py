7d91e151507dd2456ffb60336c389da8c0c4c080
nrouyer <rouyer.nicolas@gmail.com>
2024-01-29 15:55:21 +0100

    Update creer_observatoire.py if question

--- pages/creer_observatoire.py
+++ pages/creer_observatoire.py
@@ -4,6 +4,10 @@ from neo4j import GraphDatabase
 # Recherche de base : chercher Google sur Google...
 from googlesearch import search
 
+# Function to add an event to the Neo4j database
+    def add_event(session, url, description):
+        session.run("MERGE (e:Event {url: $url}) ON CREATE SET e.description = $description", url=url, description=description)
+
 st.title("📝 Créer un nouvel observatoire sur les accidents")
 
 question = st.text_input(
@@ -12,26 +16,23 @@ question = st.text_input(
 )
 
 #term = "accident+moto+grievement"
-
-results = search(question, lang="fr", num_results=50, advanced=True)
-
-
-# Neo4j connection details
-url = st.secrets["AAA_URI"]
-username = st.secrets["AAA_USERNAME"]
-password = st.secrets["AAA_PASSWORD"]
-
-# Function to add an event to the Neo4j database
-def add_event(session, url, description):
-    session.run("MERGE (e:Event {url: $url}) ON CREATE SET e.description = $description", url=url, description=description)
-
-# Create a driver instance
-driver = GraphDatabase.driver(url, auth=(username, password))
-
-# Insert data from the DataFrame
-with driver.session() as session:
-    for result in results:
-        add_event(session, result.url, result.description)
-
-# Close the driver
-driver.close()
+if question:
+    st.write('Recherche avec les termes : ', question)
+    
+    results = search(question, lang="fr", num_results=50, advanced=True)
+        
+    # Neo4j connection details
+    url = st.secrets["AAA_URI"]
+    username = st.secrets["AAA_USERNAME"]
+    password = st.secrets["AAA_PASSWORD"]
+    
+    # Create a driver instance
+    driver = GraphDatabase.driver(url, auth=(username, password))
+    
+    # Insert data from the DataFrame
+    with driver.session() as session:
+        for result in results:
+            add_event(session, result.url, result.description)
+    
+    # Close the driver
+    driver.close()
