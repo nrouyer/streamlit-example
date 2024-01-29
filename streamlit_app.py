ca17f8d1746655133ec7ce5df1e7a464dd05635a
nrouyer <rouyer.nicolas@gmail.com>
2024-01-29 20:01:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,8 @@ import neo4j as neo4j
 import googlesearch as googlesearch
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Observatoire des accidents
 """
+
+st.image('chatgpt_accident.jpg', caption='Accidents')
 
