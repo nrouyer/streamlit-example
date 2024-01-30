342be4ec24021fc62434a946beb31289fd1c6933
nrouyer <rouyer.nicolas@gmail.com>
2024-01-30 11:32:55 +0100

    Update streamlit_app.py llm questions

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import pandas as pd
 import streamlit as st
 import neo4j as neo4j
 import googlesearch as googlesearch
+import langchain
+import langchain_community
+import os
 
 """
 # Observatoire des accidents
@@ -11,3 +14,52 @@ import googlesearch as googlesearch
 
 st.image('images/chatgpt_accident.png', caption='Accidents')
 
+openai_api_key = st.secrets["OPENAI_KEY"]
+
+from langchain_community.llms import OpenAI
+from langchain_community.graphs import Neo4jGraph
+from langchain.chains import RetrievalQA
+from langchain_community.chat_models import ChatOpenAI
+from langchain.vectorstores.neo4j_vector import Neo4jVector
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_KEY"]
+url = st.secrets["NEO4J_URI"]
+username = st.secrets["NEO4J_USERNAME"]
+password = st.secrets["NEO4J_PASSWORD"]
+graph = Neo4jGraph(
+    url=url,
+    username=username,
+    password=password
+)
+
+
+llm = OpenAI()
+
+vectorstore = Neo4jVector.from_existing_graph(
+    OpenAIEmbeddings(),
+    url=url,
+    username=username,
+    password=password,
+    index_name='eventdescription',
+    node_label="Event",
+    text_node_properties=['description'],
+    embedding_node_property='embedding',
+)
+
+vector_qa = RetrievalQA.from_chain_type(
+    llm=ChatOpenAI(), chain_type="stuff", retriever=vectorstore.as_retriever())
+
+# Streamlit layout with tabs
+container = st.container()
+question = container.text_input("**:blue[Question:]**", "")
+
+if question:
+    tab1, tab2 = st.tabs(["No-RAG", "Basic RAG"])
+    with tab1:
+        st.markdown("**:blue[No-RAG.] LLM only. Purely generated answer:**")
+        st.write(llm(question))
+    with tab2:
+        st.markdown("**:blue[Basic RAG.] Answer from vector search only:**")
+        st.write(vector_qa.run(question))
+    
