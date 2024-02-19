06ec89f2d5167142edba204f8e8ae4bfce1f50fe
Nicolas Rouyer <rouyer.nicolas@gmail.com>
2024-02-19 17:09:08 +0100

    modifier ingestion de l'evenement

--- pages/nouvel_observatoire.py
+++ pages/nouvel_observatoire.py
@@ -71,14 +71,14 @@ prompt1="""Depuis la description de l'accident ci-dessous, extraire les entités
 1. Tout d'abord, recherchez ces types d'entités dans le texte et générez-les dans un format séparé par des virgules, similaire à celui des types d'entités. La propriété `id` de chaque entité doit être alphanumérique et unique parmi les entités. Vous ferez référence à cette propriété pour définir la relation entre les entités. Ne créez pas de nouveaux types d'entités qui ne sont pas mentionnés ci-dessous. Le document doit être résumé et stocké dans l'entité Article sous la propriété `description`. Vous devrez générer autant d'entités que nécessaire selon les types ci-dessous :
     Types d'entités :
     label:'Evenement',id:string,description:string,date:datetime,duree:string,lieu:string //Evenement c'est un événement qui s'est produit, par exemple un accident
-    label:'TypeEvenement',id:string,type:string //TypeEvenement c'est le type d'événement qui s'est produit
+    label:'TypeEvenement',type:string //TypeEvenement c'est le type d'événement qui s'est produit
     label:'Article',id:string,urlMedia:string,uri:string,url:string,journaliste:string,synthese:string,date:datetime,titre:string,media:string,description:string //Entité Article ; la propriété `id` c'est le nom de l'article, en lowercase & camel-case & qui commence toujours par un caractère alphabétique
     label:'Document',id:string,description:string //Entité Document ; la propriété `id` c'est le nom de du document, en lowercase & camel-case & qui commence toujours par un caractère alphabétique
-    label:'Facteur',id:string,name:string // Entité Facteur c'est le facteur explicatif de l'événement; la propriété `id` c'est le nom du facteur, en lowercase & camel-case & qui commence toujours par un caractère alphabétique
-    label:'Solution',id:string,name:string,description:string,when:string // Entité Solution c'est la solution qui pourrait aider à résoudre l'événement qui s'est produit ; la propriété `id` c'est le nom du facteur, en lowercase & camel-case & qui commence toujours par un caractère alphabétique
-    label:'Impact',id:string,name:string,description:string // Entité Impact c'est l'impact de l'événement qui s'est produit ; la propriété `id` c'est le nom de l'impact, en lowercase & camel-case & qui commence toujours par un caractère alphabétique
+    label:'Facteur',name:string // Entité Facteur c'est le facteur explicatif de l'événement; la propriété `name` c'est le nom du facteur, en lowercase & camel-case & qui commence toujours par un caractère alphabétique
+    label:'Solution',name:string,description:string,when:string // Entité Solution c'est la solution qui pourrait aider à résoudre l'événement qui s'est produit ; la propriété `name` c'est le nom du facteur, en lowercase & camel-case & qui commence toujours par un caractère alphabétique
+    label:'Impact',name:string,description:string // Entité Impact c'est l'impact de l'événement qui s'est produit ; la propriété `name` c'est le nom de l'impact, en lowercase & camel-case & qui commence toujours par un caractère alphabétique
     label:'Personne',id:string,prenom:string,nom:string,age:string,sexe:string,nationalite:string,profession:string,passeJudiciare:string // Entité Personne c'est une personne liée à l'événement qui s'est produit ; la propriété `id` c'est le nom de la personne, en lowercase & camel-case & qui commence toujours par un caractère alphabétique
-    label:'Groupe',id:string,nom:string,nature:string,nombreMembres:integer // Entité Groupe c'est un groupe auquel une personne est liée ; la propriété `id` c'est le nom du groupe, en lowercase & camel-case & qui commence toujours par un caractère alphabétique
+    label:'Groupe',nom:string,nature:string,nombreMembres:integer // Entité Groupe c'est un groupe auquel une personne est liée ; la propriété `name` c'est le nom du groupe, en lowercase & camel-case & qui commence toujours par un caractère alphabétique
 2. Ensuite, générez chaque relation comme un triplet de source, relation et cible. Pour faire référence à l'entité source et à l'entité cible, utilisez leur propriété `id` respective. Vous devrez générer autant de relations que nécessaire, comme défini ci-dessous :
     Types de relations :
     Personne|A_BLESSE|Personne
@@ -115,6 +115,8 @@ $ctext
 """
 
 openai.api_key = st.secrets["OPENAI_KEY"]
+
+nbResSearch = st.secrets["NB_RES_SEARCH"]
 
 # GPT-4 or GPT-3.5 Prompt to complete
 @retry(tries=2, delay=5)
@@ -263,7 +265,7 @@ question = st.text_input(
 if question:
     st.write('Recherche avec les termes : ', question)
     
-    results = search(question, lang="fr", num_results=25, advanced=True)
+    results = search(question, lang="fr", num_results=10, advanced=True)
         
     # Neo4j connection details
     url = st.secrets["AAA_URI"]
