29197f12957863a1227dab7812b31fc9510ecc0f
Nicolas Rouyer <rouyer.nicolas@gmail.com>
2024-02-20 00:48:21 +0100

    correction requête cypher facteurs 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,9 +68,9 @@ vector_qa = RetrievalQA.from_chain_type(
 contextualize_query = """
 match (node)-[:DOCUMENTE]->(e:Evenement)
 WITH node AS a, e, score, {} as metadata limit 1
-OPTIONAL MATCH (e)<-[:EXPLIQUE]-(f:Facteur)-[:EXPLIQUE]->(e2:Evenement)
-WITH a, e, f, score, metadata, e2
-RETURN "Evenement : "+ e.description + " facteurs explicatifs : " + coalesce(f.name, "") + " evenement similaire : " + coalesce(e2.description) +"\n" as text, score, metadata
+OPTIONAL MATCH (e)<-[:EXPLIQUE]-(f:Facteur)-[:EXPLIQUE]->(:Evenement)
+WITH a, e, score, metadata, apoc.text.link(f.name, ",") AS facteurs
+RETURN "Evenement : "+ e.description + " facteurs explicatifs : " + coalesce(facteurs, "") +"\n" as text, score, metadata
 """
 
 contextualize_query1 = """
