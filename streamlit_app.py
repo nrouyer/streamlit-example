55f5d96cf1fc05440c3317050114157bf8a43d77
Nicolas Rouyer <rouyer.nicolas@gmail.com>
2024-02-20 00:32:28 +0100

    ajout contexte facteurs

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,8 +69,8 @@ contextualize_query = """
 match (node)-[:DOCUMENTE]->(e:Evenement)
 WITH node AS a, e, score, {} as metadata limit 1
 OPTIONAL MATCH (e)<-[:EXPLIQUE]-(f:Facteur)-[:EXPLIQUE]->(:Evenement)<-[:DOCUMENTE]-(a2:Article)
-WITH a, e, score, metadata, apoc.text.join(collect(f.name), ",") AS facteurs,  apoc.text.join(collect(a2.titre), "\n") AS articles
-RETURN "Titre Article: "+ a.titre + " ,description: "+ a.description + " facteurs explicatifs : " + coalesce(facteurs, "") + "\nautres evenements mêmes facteurs: "+ coalesce(articles, "") +"\n" as text, score, metadata
+WITH a, e, score, metadata, apoc.text.join(collect(f.name), ",") AS facteurs
+RETURN "Titre Article: "+ a.titre + " ,description: "+ a.description + " facteurs explicatifs : " + coalesce(facteurs, "") +"\n" as text, score, metadata
 """
 
 contextualize_query1 = """
